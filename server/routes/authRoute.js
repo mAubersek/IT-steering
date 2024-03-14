6699c47e3cb79da90f7a1c07e83b8c6e58b72e39
Matjaž Auberšek <ma4481@student.uni-lj.si>
2024-03-15 00:53:02 +0100

    Dodan middleware za authentication

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,10 @@
 const { Signup, Login } = require("../Controllers/authController");
+const { userVerification } = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
-router.post('/login', Login)
+router.post('/login', Login);
+router.post('/',userVerification);
 
 
 module.exports = router;
