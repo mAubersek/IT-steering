02bc2f7bb8a1046ff91fdb9533162f4edba17a01
Matjaž Auberšek <ma4481@student.uni-lj.si>
2024-03-15 09:46:35 +0100

    Osnovni frontend routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Register, Home } from "./pages"
 
 function App() {
-  return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
